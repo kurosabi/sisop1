ad95ec107e5a4c0908f5e4654169a05cc0f0f6f7
kurosabi <38504443+kurosabi@users.noreply.github.com>
2022-09-06 23:45:53 -0500

    version final

--- proyectosis1.c
+++ proyectosis1.c
@@ -6,184 +6,199 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <limits.h>
-// CONSTANTS
-#define MAX_LINE 80 // The maximum length command
 
-// Array which will save the last 10 commands
+#define MAX_LINE 80 /* The maximum length command */
+
+
 char historial[10][MAX_LINE];
 int argCount = 0;
 
 // Agraga los comandos ingresados al arreglo historial que es manejado como una pila
-void add_into_arr(char *string) {
+void add_historial(char *string){
 	
-	for (int i = 9; i > 0; --i)
+	for (int i = 9; i > 0; --i){
+
     	strcpy(historial[i], historial[i - 1]);
-        
+    }  
+
 	strcpy(historial[0], string);
 	++argCount;
 }
 
 // Organiza los comandos ingresados separados para facilitar la revision de la correctitud de los comandos escritos
 // mete los comando en una lista de strings
-bool tokenize(char *string, char **ret) {
+bool purgarInput(char *string, char **aux){
 	
 	int i = 0;
-	char *token[41];
-	token[0] = strtok(string, " ");
+	char *comando[41];
+	comando[0] = strtok(string, " ");
 	
-	while(token[i] != NULL) {
+	while(comando[i] != NULL){
 		
-		ret[i] = token[i];
-		token[++i] = strtok(NULL, " ");
+		aux[i] = comando[i];
+		comando[++i] = strtok(NULL, " ");
 	}
 	
 	char arr1[10];
-	strcpy(arr1, ret[i - 1]);
-	token[i] = strtok(ret[i - 1], "&");
+	strcpy(arr1, aux[i - 1]);
+	comando[i] = strtok(aux[i - 1], "&");
 	
-	if(strcmp(token[i], arr1) == 0) {
+	if(strcmp(comando[i], arr1) == 0){
 		
-		ret[i] = NULL;
+		aux[i] = NULL;
 		return false;
 	}
 	
-	else {
+	else{
 		
-		ret[i - 1] = token[i];
-		ret[i] = NULL;
+		aux[i - 1] = comando[i];
+		aux[i] = NULL;
 		return true;
 	}
 }
 
 // Muestra el historial de comandos 
-void show_history() {
+void mostrar_historial(){
 	
-	if(argCount == 0)
+	if(argCount == 0){
+
 		printf("No command has been entered so far.\n");
+    }
 	
-	else if(argCount < 11)
-		for(int i = 0; i < argCount; ++i)
+	else if(argCount < 11){
+
+		for(int i = 0; i < argCount; ++i){
+
 			printf("%d\t%s\n", argCount - i, historial[i]);
-	
-	else
-		for(int i = 0; i < 10; ++i)
+        }
+    }
+
+	else{
+
+		for(int i = 0; i < 10; ++i){
+            
 			printf("%d\t%s\n", argCount - i, historial[i]);
+        }
+    }
 }
 
-// Main Function 
+
+
 int main(void){
 	 
-
-	char *args[MAX_LINE/2 + 1]; // Command line arguments
-	int should_run = 1; // Flag to determine when to exit program
+	char *args[MAX_LINE/2 + 1]; /* Command line arguments */
+	int should_run = 1; /* Flag to determine when to exit program */
 	
 	while (should_run){
 
 		char cwd[PATH_MAX];
 		getcwd(cwd, sizeof(cwd));
-		char scanned_input[50];
+		char entrada[50];
 		char *arg;
 		
 		printf("\n%s\n",cwd);
 		printf("osh>");
 		fflush(stdout);
-		scanf("%[^\n]", scanned_input); // Command + Arguments input stream
+		scanf("%[^\n]", entrada);
         getchar();
         
 		
 		bool ultimoAmpersand;
 
 		// Muestra el historial de comando si se escribe el comando history
-		if(strcmp(scanned_input, "history") == 0) {
+		if(strcmp(entrada, "historial") == 0){
 			
-			show_history(); 
+			mostrar_historial(); 
 			continue; 
 		}
 
 		// trae el comando ejecutado mas reciente
-		if(scanned_input[0] == '!') {
+		if(entrada[0] == '!'){
 			
-			if(argCount == 0) {
+			if(argCount == 0){
 				
 				printf("No recent arguments to execute\n");
 				continue;
 			}
 
             // trae el primer comando mas raciente con !!
-			if(scanned_input[1] == '!') { 
+			if(entrada[1] == '!'){ 
 
-				add_into_arr(historial[0]);
-				
-				char s1[MAX_LINE];
-				strcpy(s1,historial[0]);
-				ultimoAmpersand = tokenize(s1, args);
-				
-				char com[MAX_LINE];
-				strcpy(com,historial[0]);
-				ultimoAmpersand = tokenize(com, args);
+				add_historial(historial[0]);
+				char temp[MAX_LINE];
+				strcpy(temp,historial[0]);
+				ultimoAmpersand = purgarInput(temp, args);
+				char temp2[MAX_LINE];
+				strcpy(temp2,historial[0]);
+				ultimoAmpersand = purgarInput(temp2, args);
 			}
 			
             // trae n-esimo comando mas reciente con !#
-			else {
+			else{
 				
-				int n = scanned_input[1] - 48;
+				int n = entrada[1] - 48;
 				
-				if(n >= 11 || n <= 0) {
-				printf("Only 10 commands can be displayed\n");
+				if(n >= 11 || n <= 0){
+
+				    printf("Only 10 commands can be displayed\n");
 					continue;
 				}
 
-				add_into_arr(historial[n]);
-				
-				char s1[MAX_LINE];
-				strcpy(s1,historial[n]);
-				ultimoAmpersand = tokenize(s1, args);
-				
-				char com[MAX_LINE];
-				strcpy(com,historial[n]);
-				ultimoAmpersand = tokenize(com, args);
+				add_historial(historial[n]);
+				char temp[MAX_LINE];
+				strcpy(temp,historial[n]);
+				ultimoAmpersand = purgarInput(temp, args);
+				char temp2[MAX_LINE];
+				strcpy(temp2,historial[n]);
+				ultimoAmpersand = purgarInput(temp2, args);
 			}
 		}
 		
         // recibe un comando normal
-		else {	
+		else{	
 
-			add_into_arr(scanned_input);
-			ultimoAmpersand = tokenize(scanned_input, args);
+			add_historial(entrada);
+			ultimoAmpersand = purgarInput(entrada, args);
 		}
 
 		// Crea un proceso hijo 
-		pid_t child_pid;
-        int status;
-		child_pid = fork();
+		pid_t childPid;
+		childPid = fork();
 
 		// para salir del programa
-		if(strcmp(*(args), "exit") == 0)			
+		if(strcmp(*(args), "exit") == 0){
+
 			exit(0);
-		
+        }
+
         // para entrar en una ubicacion de una carpeta
-		if(strcmp(args[0], "cd") == 0) {
+		if(strcmp(args[0], "cd") == 0){
+
 			char *loc_string = args[1];
 			int found = chdir(loc_string);
-			if(found < 0)
+
+			if(found < 0){
+
 				printf("Location not found.\n");
-			
+            }
+
 			continue;
 		}		
 		
         // ejecutan los comandos recibidos en el proceso hijo
-		if(child_pid == 0) {
-			
-			
+		if(childPid == 0){
+						
 			printf("\n");
 			execvp(args[0], args);
 			fprintf (stderr,"\nCommand not recognised.\n");
-			exit(1);
+			abort();
 		}
 
 		// si no encuentra el ampersand el proceso padre espera al proceso hijo para continuar la ejecucion.
-		if(ultimoAmpersand == false)
-			waitpid(child_pid, &status, WNOHANG);
+		if(ultimoAmpersand == false){
+			wait(NULL);
+        }
 	}
+    
 	return 0;
 }
