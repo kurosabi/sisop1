0c28708132348b5021ef7d076785aebc17cb374a
kurosabi <38504443+kurosabi@users.noreply.github.com>
2022-09-09 14:55:14 -0500

    commit

--- proyectosis1.c
+++ proyectosis1.c
@@ -194,7 +194,7 @@ int main(void){
 			printf("\n");
 			execvp(args[0], args);
 			fprintf (stderr,"\nCommand not recognised.\n");
-			exit(0);
+			abort();
 		}
 
 		// si no encuentra el ampersand el proceso padre espera al proceso hijo para continuar la ejecucion.
